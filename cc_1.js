9c6db5cf65f93a17ddd2716b2dd98b3bf0f74ee2
Martin Leone <martinnikolaslll@gmail.com>
2024-08-30 15:34:59 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,21 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+
+
+
+//Task 2: Compound Data Types
+
+let products = ["Paper, Stapler, Markers"]
+const productDetails = {
+name: products,
+price: 6.60,
+inStock: 720
+
+
+}
+
+console.log(products, productDetails);
+
+
